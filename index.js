3abaf1ed5024b9bb81cea55bf00f35e1c76ab3e7
YeniUvwo <125455423+YeniUvwo@users.noreply.github.com>
2023-07-20 11:28:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.icon-menu-close').click(function(){
+        $('.dropdown-content').toggle();
+    })
+
+    $('.menu-icon').click(function(){
+        $('.dropdown-content').toggle();
+    })
+})
